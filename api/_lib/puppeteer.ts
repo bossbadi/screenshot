2c16c9ee6456ffee850d9d13466636f9a5e488aa
Andy Tang <bossbadi123@gmail.com>
2023-07-07 18:59:10 -0400

    Update puppeteer.ts

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -16,7 +16,7 @@ async function getPage() {
 
 export async function getScreenshot(url, width, height) {
     const page = await getPage();
-    await page.goto(url);
+    await page.goto(url, {"waitUntil" : "networkidle0"});
     await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720, deviceScaleFactor: 2 });
     const file = await page.screenshot();
     return file;
