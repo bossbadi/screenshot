393bfb1d71e87ba96efba37174fb2876bd3bf6b6
Andy Tang <bossbadi123@gmail.com>
2023-07-08 00:11:09 +0000

    imagesHaveLoaded

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -3,23 +3,29 @@ import chrome from "chrome-aws-lambda";
 let _page: Page | null;
 
 async function getPage() {
-    if (_page) return _page;
-    const options = { 
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-    };
-    const browser = await launch(options);
-    _page = await browser.newPage();
-    return _page;
+  if (_page) return _page;
+  const options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  };
+  const browser = await launch(options);
+  _page = await browser.newPage();
+  return _page;
 }
 
 export async function getScreenshot(url, width, height, delay) {
-    const page = await getPage();
-    await page.setViewport({ width: Number(width) || 1920, height: Number(height) || 1080});
+  const page = await getPage();
+  await page.setViewport({ width: Number(width) || 1920, height: Number(height) || 1080 });
 
-    await page.goto(url, { waitUntil: "networkidle0" });
+  await page.goto(url, { waitUntil: "networkidle0" });
+  await page.waitForFunction(imagesHaveLoaded);
 
-    const file = await page.screenshot({fullPage: true});
-    return file;
+  const file = await page.screenshot({ fullPage: true });
+
+  return file;
+}
+
+function imagesHaveLoaded() {
+  return Array.from(document.images).every((i) => i.complete);
 }
